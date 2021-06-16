ab4388740d18d4238f17f1feb42e5904f0dc9f2e
Dangit1201 <dangit1201@gmail.com>
2021-06-17 06:55:08 +0700

    lam cn category

--- src/routers/web.js
+++ src/routers/web.js
@@ -6,6 +6,7 @@ const CategoryController = require("../app/controllers/category");
 const AdminController = require("../app/controllers/admin");
 //===========admin================
 router.get("/admin",AdminController.index);
+
 //===========Category===============
 router.get("/admin/categories",CategoryController.index);
 router.get("/admin/categories/create",CategoryController.create);
@@ -14,5 +15,14 @@ router.get("/admin/categories/edit/:id",CategoryController.edit);
 router.post("/admin/categories/update/:id",CategoryController.update);
 router.post("/admin/categories/delete/:id",CategoryController.dele);
 router.post("/admin/categories/reorder",CategoryController.reorder);
+
+//===========Product===============
+router.get("/admin/products",CategoryController.index);
+router.get("/admin/products/create",CategoryController.create);
+router.post("/admin/products/store",CategoryController.store);
+router.get("/admin/products/edit/:id",CategoryController.edit);
+router.post("/admin/products/update/:id",CategoryController.update);
+router.post("/admin/products/delete/:id",CategoryController.dele);
+
 
 module.exports = router;
